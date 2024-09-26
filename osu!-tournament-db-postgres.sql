be5de715fe04c74e114504f4e35f909b7d379e81
rrailgun <rrailgun456@gmail.com>
2024-09-26 00:07:41 -0400

    Add Postgres SQL and ERD

---
+++ osu!-tournament-db-postgres.sql
@@ -0,0 +1,62 @@
+CREATE TABLE "players" (
+  "player_id" varchar PRIMARY KEY,
+  "username" varchar,
+  "team_acr" char(3)
+);
+
+CREATE TABLE "teams" (
+  "acronym" char(3) PRIMARY KEY,
+  "teamname" varchar UNIQUE NOT NULL,
+  "seed" integer
+);
+
+CREATE TABLE "tourney_round" (
+  "round_name" varchar PRIMARY KEY,
+  "round_acronym" varchar NOT NULL
+);
+
+CREATE TABLE "maps" (
+  "beatmap_id" varchar PRIMARY KEY,
+  "tourney_round_name" varchar,
+  "slot_name" varchar
+);
+
+CREATE TABLE "mappools" (
+  "slot_name" varchar PRIMARY KEY,
+  "mod_acronym" varchar
+);
+
+CREATE TABLE "matches" (
+  "match_id" integer PRIMARY KEY,
+  "mp_id" varchar,
+  "team1_acr" char(3),
+  "team2_acr" char(3),
+  "tourney_round_name" varchar,
+  "winning_team" varchar
+);
+
+CREATE TABLE "player_scores" (
+  "match_id" integer,
+  "beatmap_id" varchar,
+  "player_id" varchar,
+  "score" integer NOT NULL,
+  PRIMARY KEY ("match_id", "beatmap_id", "player_id")
+);
+
+ALTER TABLE "players" ADD FOREIGN KEY ("team_acr") REFERENCES "teams" ("acronym");
+
+ALTER TABLE "maps" ADD FOREIGN KEY ("tourney_round_name") REFERENCES "tourney_round" ("round_name");
+
+ALTER TABLE "maps" ADD FOREIGN KEY ("slot_name") REFERENCES "mappools" ("slot_name");
+
+ALTER TABLE "matches" ADD FOREIGN KEY ("team1_acr") REFERENCES "teams" ("acronym");
+
+ALTER TABLE "matches" ADD FOREIGN KEY ("team2_acr") REFERENCES "teams" ("acronym");
+
+ALTER TABLE "matches" ADD FOREIGN KEY ("tourney_round_name") REFERENCES "tourney_round" ("round_name");
+
+ALTER TABLE "player_scores" ADD FOREIGN KEY ("match_id") REFERENCES "matches" ("match_id");
+
+ALTER TABLE "player_scores" ADD FOREIGN KEY ("beatmap_id") REFERENCES "maps" ("beatmap_id");
+
+ALTER TABLE "player_scores" ADD FOREIGN KEY ("player_id") REFERENCES "players" ("player_id");
