aadaf59faaab7fb6a6e00c5723ffb648f631c95c
rrailgun <rrailgun456@gmail.com>
2024-09-28 16:22:18 -0400

    remove unused dependencies, add player reg

--- src/server.js
+++ src/server.js
@@ -3,15 +3,25 @@ import { config } from 'dotenv';
 import passport from 'passport';
 import OAuth2Strategy from 'passport-oauth2';
 import session from 'express-session';
-config();
+import { Client } from 'osu-web.js';
+import pgPromise from 'pg-promise';
 
+config();
+const pgp = pgPromise();
+const db = pgp({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
 const app = express();
 app.use(session({
-    secret: 'keyboard cat',
+    secret: process.env.COOKIE_KEY,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
-  }));
+    cookie: { secure: true }
+}));
 app.use(passport.session())
 
 passport.use(new OAuth2Strategy({
@@ -20,39 +30,67 @@ passport.use(new OAuth2Strategy({
     clientID: process.env.OSU_CLIENT_ID,
     clientSecret: process.env.OSU_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/cb"
-  },
-  function(_accessToken, _refreshToken, profile, cb) {
-    return cb(null, {
-        token: _accessToken,
-        username: 'test'
-    });
-  }
+},
+    function (_accessToken, _refreshToken, profile, cb) {
+        let api = new Client(_accessToken);
+        api.users.getSelf().then(res => {
+            return cb(null, {
+                token: _accessToken,
+                refreshToken: _refreshToken,
+                username: res.username
+            });
+        })
+    }
 ));
 
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, {
-        'token': user.token,
-        'username': user.username
-      });
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
     });
-  });
-  
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-        console.log(user);
-      return cb(null, user);
-    });
-  });
+});
 
-app.get('/', (req,res) => {
-    res.send('Server online')
-})
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
 
 app.get('/auth', passport.authenticate('oauth2'))
 
-app.get('/auth/cb', passport.authenticate('oauth2', { failureRedirect: '/' }), (req,res) => {
-    res.send('Logged in as '+req.user.username)
+app.get('/auth/cb', passport.authenticate('oauth2', { failureRedirect: '/' }), (req, res) => {
+    res.send('Logged in as ' + req.user.username)
 })
+
+/**
+ * Redirect unauthorized users to sign in
+ */
+app.use((req, res, next) => {
+    if (req.user) next()
+    else res.redirect('/auth')
+  })
+
+app.get('/', (req, res) => {
+    res.send('Server online')
+})
+
+app.post('/tournament/register', (req,res) => {
+    let api = new Client(req.user.token);
+    api.users.getSelf().then(userResponse => {
+        db.none('INSERT INTO players(player_id, username) VALUES(${id}, ${username})', {
+            id: userResponse.username,
+            username: userResponse.id
+        })
+        .then( () => res.sendStatus(201))
+        .catch( error => {
+            res.send(error);
+            res.status(400);
+        })
+    })
+    .catch( error => {
+        res.status(401);
+        res.redirect('/auth')
+    })
+})
+
 
 app.listen(process.env.PORT, () => console.log('Server up at http://localhost:3000/'))
